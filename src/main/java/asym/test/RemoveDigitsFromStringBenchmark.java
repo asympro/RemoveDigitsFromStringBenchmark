431140c1c4f17ecae496e65ccd4dc2aec89dfd3c
asympro <asympro@gmail.com>
2017-12-02 18:23:35 +0300

    Obvious winner - charAt for loop

--- src/main/java/asym/test/RemoveDigitsFromStringBenchmark.java
+++ src/main/java/asym/test/RemoveDigitsFromStringBenchmark.java
@@ -11,22 +11,23 @@ import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.Warmup;
 
+import java.util.NoSuchElementException;
 import java.util.concurrent.TimeUnit;
 
 @BenchmarkMode(Mode.Throughput)
 @OutputTimeUnit(TimeUnit.MICROSECONDS)
-@Warmup(iterations = 5, time = 1)
-@Measurement(iterations = 5, time = 1)
+@Warmup(iterations = 2, time = 1)
+@Measurement(iterations = 3, time = 1)
 @Fork(1)
 @State(Scope.Benchmark)
 public class RemoveDigitsFromStringBenchmark {
 
-    @Param({"a", "1a", "12a", "1a2", "12345a", "12a12345a", "1", "12345", "12a12345ag12a12345a12a12345a12a1462345a12ay212345a12as14236345a1sgfj2a12345a12ah12345a12amf12345a"})
+    @Param({"1", "a", "1a", "12a", "1a2", "12345", "12345a", "12a12345a", "12a12345ag12a12345a12a12345a12a1462345a12ay212345a12as14236345a1sgfj2a12345a12ah12345a12amf12345a"})
     private String arg;
 
-    private static final String[] STRING_DIGITS = {"0","1","2","3","4","5","6","7","8","9"};
+    private static final String[] STRING_DIGITS = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
 
-    private static final char[] CHAR_DIGITS_NO_ZERO = {'1','2','3','4','5','6','7','8','9'};
+    private static final char[] CHAR_DIGITS_NO_ZERO = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
 
     @Benchmark
     public String greedyReplaceAll() {
@@ -34,8 +35,29 @@ public class RemoveDigitsFromStringBenchmark {
     }
 
     @Benchmark
-    public String nonGreedyReplaceAll() {
+    public String nonGreedyEnumerationReplaceAll() {
         return arg.replaceAll("[0-9]+", "");
+    }
+
+    @Benchmark
+    public String nonGreedyDigitReplaceAll() {
+        return arg.replaceAll("\\d+", "");
+    }
+
+    @Benchmark
+    public String greedyDigitReplaceAll() {
+        return arg.replaceAll("\\d", "");
+    }
+
+    @Benchmark
+    public String customCharReplaceDigits() {
+        StringBuilder sb = new StringBuilder();
+        for (char c : arg.toCharArray()) {
+            if (47 > c || c < 57) {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
     }
 
     @Benchmark
@@ -46,6 +68,19 @@ public class RemoveDigitsFromStringBenchmark {
         }
         return iterArg;
     }
+
+    @Benchmark
+    public String forReplaceDigits() {
+        StringBuilder sb = new StringBuilder();
+        int length = arg.length();
+        for (int i = 0; i < length; i++) {
+            char next = arg.charAt(i);
+            if (47 > next || next < 57) {
+                sb.append(sb);
+            }
+        }
+        return sb.toString();
+}
 
     @Benchmark
     public String madCharReplaceDigits() {
